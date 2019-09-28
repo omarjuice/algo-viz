13d5744d6832c7a4f7a9f297675e7a78521a1356
Omar Juice <ojameer1@gmail.com>
2019-09-28 18:13:31 -0400

    docker setup for js image

--- runner/js/execute/index.js
+++ runner/js/execute/index.js
@@ -1,30 +1,31 @@
-const { Worker } = require('worker_threads')
-const transpile = require('../transpile')
-const fs = require('fs')
-const prod = process.env.NODE_ENV === 'production'
-module.exports = async function (code) {
-    const input = { _name: null, references: {} }
-    const transpiled = await transpile(code, input)
-    !prod && fs.writeFile('transpiled.js', transpiled, () => { })
-    const worker = new Worker(__dirname + '/thread.js', {
-        workerData: {
-            code: transpiled,
-            _name: input._name,
-            original: code,
-            prod,
-            timeout: Number(process.env.EXECUTION_TIMEOUT) || 500,
-            version: process.env.DATA_VERSION
-        }
-    })
-    return new Promise((resolve, reject) => {
-        worker.on('message', data => {
-            !prod && fs.writeFile('executed.json', data, () => { })
-            resolve(data)
-            worker.terminate()
-        })
-        worker.on('error', error => {
-            worker.terminate();
-            reject(error)
-        })
-    })
+// const print = v => { return console.log(v), v };
+module.exports = function () {
+    let ___name;
+    let ___code;
+    let Viz;
+    eval((function (func) {
+        ___code = func;
+        const fs = require('fs')
+        const input = { _name: null, references: {} }
+        const code = require('../transpile')(func, input)
+        const { _name } = input
+        global[_name] = new (require('./runner'))(_name, func)
+        const { default: instantiateViz } = require('../builtins/dist');
+        Viz = instantiateViz(global[_name])
+        !process.env.NODE_ENV === 'production' && fs.writeFileSync('transpiled.js', code)
+        ___code = func
+        return code + `\n___name = '${_name}';`
+    })(process.env.CODE))
+    console.log('NUMBER OF STEPS ', global[___name].steps.length);
+    const fs = require('fs')
+    const { types, objects, steps, objectIndex } = global[___name]
+    delete global[___name]
+    !process.env.NODE_ENV === 'production' && fs.writeFileSync('executed.json', JSON.stringify({
+        steps,
+        objects,
+        types,
+        objectIndex,
+        code: ___code
+    }))
+    console.log({ steps, objects, types, objectIndex });
 }
